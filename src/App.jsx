37478b973470f04cbae09d7b48e0d74857cb5ee2
Umar2468-tech <areebfoodcorner@gmail.com>
2025-02-12 03:15:44 +0500

    Random-Color-Generator by Reactjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Accoedion from "./component/accordion/Accoedion";
+import Random from "./component/Randomcolor/Random";
 
 const App = () => {
   return (
     <div>
       <div>
-        <Accoedion />
+        <Random />
       </div>
     </div>
   );
