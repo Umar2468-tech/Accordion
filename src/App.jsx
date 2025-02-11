e0da3ab1ed35ab9d7e36fa57b075e49b9b6cab9c
Umar2468-tech <areebfoodcorner@gmail.com>
2025-02-12 03:06:36 +0500

    accordion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Accoedion from "./component/accordion/Accoedion";
+import Random from "./component/Randomcolor/Random";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        {/* <Accoedion /> */}
+        <Random />
+      </div>
+    </div>
+  );
+};
+
+export default App;
